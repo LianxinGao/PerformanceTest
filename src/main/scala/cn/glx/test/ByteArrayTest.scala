4cca4e792af6e2700112cb631b6070a8978f3d49
LianxinGao <503773453@qq.com>
2022-02-22 09:18:24 +0800

    Initial commit

---
+++ src/main/scala/cn/glx/test/ByteArrayTest.scala
@@ -0,0 +1,19 @@
+package cn.glx.test
+
+/**
+ * @program: StorageTest
+ * @description:
+ * @author: LiamGao
+ * @create: 2022-02-22 09:14
+ */
+class ByteArrayTest {
+  def initDB(): Unit ={
+
+  }
+  def testByteArrayWrite(): Unit ={
+
+  }
+  def testByteArrayRead(): Unit ={
+    
+  }
+}
